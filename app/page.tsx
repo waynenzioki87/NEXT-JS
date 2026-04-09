7213550ae055554bc4e044aaf1a3d43d7d0caadb
Wayne-Nzioki <waynenzioki87@gmail.com>
2026-04-09 12:26:05 +0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
+          className="light:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
